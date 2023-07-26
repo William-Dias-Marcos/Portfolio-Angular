2e89b01a1414115a953d2afe40c8ebfaf59cc0dd
William-Dias-Marcos <william.diasmarcos@gmail.com>
2023-07-26 09:02:41 -0300

    adjust in navigation bar

--- src/app/components/navigation-bar/navigation-bar.component.ts
+++ src/app/components/navigation-bar/navigation-bar.component.ts
@@ -3,8 +3,12 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-navigation-bar',
   templateUrl: './navigation-bar.component.html',
-  styleUrls: ['./navigation-bar.component.css']
+  styleUrls: ['./navigation-bar.component.css'],
 })
 export class NavigationBarComponent {
-
+  hideNav(valor: HTMLInputElement) {
+    setTimeout(() => {
+      valor.checked = false;
+    }, 100);
+  }
 }
