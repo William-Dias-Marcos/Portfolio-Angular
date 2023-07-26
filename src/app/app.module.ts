0cb1cb2f2fdceafd6d1875f6c80d4effb7adbcc3
William-Dias-Marcos <william.diasmarcos@gmail.com>
2023-07-26 16:56:10 -0300

    created footer

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,13 +5,15 @@ import { AppComponent } from './app.component';
 import { NavigationBarComponent } from './components/navigation-bar/navigation-bar.component';
 import { HomeScreenComponent } from './components/home-screen/home-screen.component';
 import { ProjectsComponent } from './components/projects/projects.component';
+import { FooterComponent } from './components/footer/footer.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavigationBarComponent,
     HomeScreenComponent,
-    ProjectsComponent
+    ProjectsComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule
