4e0e639b1c185bbee185ac6da83550c0949336cf
William-Dias-Marcos <william.diasmarcos@gmail.com>
2025-02-13 21:32:03 -0300

    feat: adjustment in component about, added theme control

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,14 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{html,ts}"],
+  content: ['./src/**/*.{html,ts}'],
   theme: {
     extend: {},
+    fontFamily: {
+      sans: ['"Roboto"', 'sans-serif'],
+    },
   },
-  plugins: [require("daisyui")],
+  plugins: [require('daisyui')],
+  daisyui: {
+    themes: ['fantasy', 'dracula'],
+  },
 };
